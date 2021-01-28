595d0d8c1f7261dd6c80c7c47628c9f9428a47fe
tmeteyer <theo.mty@outlook.com>
2021-01-28 15:41:40 +0100

    nouvelles fixtures

--- Code/src/DataFixtures/AppFixtures.php
+++ Code/src/DataFixtures/AppFixtures.php
@@ -15,40 +15,42 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+
+        /******************************************/
+        /****************ADRESSES******************/
+        /******************************************/
+
         $adresse1 = new Adresse();
-        $adresse1->setRue('Rue de Amiens');
-        $adresse1->setNumero(1);
-        $adresse1->setCodePostal(80000);
-        $adresse1->setVille('Amiens');
+        $adresse1->setRue('Rue du chateau');
+        $adresse1->setNumero(10);
+        $adresse1->setCodePostal(60860);
+        $adresse1->setVille('St Omer en Chaussée');
         $manager->persist($adresse1);
 
         $adresse2 = new Adresse();
-        $adresse2->setRue('Rue de Paris');
-        $adresse2->setNumero(2);
-        $adresse2->setCodePostal(80000);
-        $adresse2->setVille('Amiens');
+        $adresse2->setRue('Rue de Maidstone');
+        $adresse2->setNumero(6);
+        $adresse2->setCodePostal(60000);
+        $adresse2->setVille('Beauvais');
         $manager->persist($adresse2);
 
         $adresse3 = new Adresse();
-        $adresse3->setRue('Route de Rouen');
-        $adresse3->setNumero(3);
-        $adresse3->setCodePostal(80000);
-        $adresse3->setVille('Amiens');
+        $adresse3->setRue('Avenue Blaise Pascal');
+        $adresse3->setNumero(41);
+        $adresse3->setCodePostal(60020);
+        $adresse3->setVille('Beauvais');
         $manager->persist($adresse3);        
 
-        $adresse4 = new Adresse();
-        $adresse4->setRue('Place du Casino');
-        $adresse4->setNumero(0);
-        $adresse4->setCodePostal(98000);
-        $adresse4->setVille('Monaco');
-        $manager->persist($adresse4);
+        $adresse3 = new Adresse();
+        $adresse3->setRue('Rue sans rue');
+        $adresse3->setNumero(0);
+        $adresse3->setCodePostal(10000);
+        $adresse3->setVille('Nowhere');
+        $manager->persist($adresse3);  
 
-        $adresse5 = new Adresse();
-        $adresse5->setRue('Grace King Pl');
-        $adresse5->setNumero(4100 );
-        $adresse5->setCodePostal(70002);
-        $adresse5->setVille('Nouvelle-Orleans');
-        $manager->persist($adresse5);
+        /******************************************/
+        /****************PERSONNES*****************/
+        /******************************************/
 
         $personne1 = new Personne();
         $personne1->setNomPrenom('Jean Mi');
@@ -59,71 +61,89 @@ class AppFixtures extends Fixture
         
         $personne2 = new Personne();
         $personne2->setNomPrenom('Jean Emile');
-        $personne2->setSexe('Z');
+        $personne2->setSexe('H');
         $personne2->setDateNaissance(new \DateTime(2010-05-20));
         $personne2->setAdresse($adresse2);
         $manager->persist($personne2);
+
+        $personne3 = new Personne();
+        $personne3->setNomPrenom('Sophie');
+        $personne3->setSexe('F');
+        $personne3->setDateNaissance(new \DateTime(2030-07-10));
+        $personne3->setAdresse($adresse2);
+        $manager->persist($personne3);
         
+        /******************************************/
+        /**************CATEGORIES******************/
+        /******************************************/
+
         $categorie1 = new categorie();
-        $categorie1->setNom('Jeux Vidéo');
+        $categorie1->setNom('Jeux de construction');
         $manager->persist($categorie1);
         
         $categorie2 = new categorie();
-        $categorie2->setNom('Jeux de société');
+        $categorie2->setNom('Sport');
         $manager->persist($categorie2);
         
         $categorie3 = new categorie();
-        $categorie3->setNom('Multimédia');
+        $categorie3->setNom('Jeux-vidéo');
         $manager->persist($categorie3);
         
         $categorie4 = new categorie();
-        $categorie4->setNom('Livre');
+        $categorie4->setNom('Créatifs');
         $manager->persist($categorie4);
         
         $categorie5 = new categorie();
-        $categorie5->setNom('Sport');
+        $categorie5->setNom('Jeux de société');
         $manager->persist($categorie5);
         
+        /******************************************/
+        /****************CADEAUX*******************/
+        /******************************************/
+
         $cadeau1 = new Cadeau();
         $cadeau1->setCategorie($categorie1);
-        $cadeau1->setDesignation('PS4');
+        $cadeau1->setDesignation('Lego');
         $cadeau1->setAgeMinimum(3);
-        $cadeau1->setPrix(500);
+        $cadeau1->setPrix(10);
         $manager->persist($cadeau1);
         
         $cadeau2 = new Cadeau();
-        $cadeau2->setCategorie($categorie2);
-        $cadeau2->setDesignation('Monopoly');
-        $cadeau2->setAgeMinimum(6);
-        $cadeau2->setPrix(20);
+        $cadeau2->setCategorie($categorie1);
+        $cadeau2->setDesignation('Playmobil');
+        $cadeau2->setAgeMinimum(3);
+        $cadeau2->setPrix(10);
         $manager->persist($cadeau2);
 
         $cadeau3 = new Cadeau();
-        $cadeau3->setCategorie($categorie3);
-        $cadeau3->setDesignation('Samsung S123');
-        $cadeau3->setAgeMinimum(10);
-        $cadeau3->setPrix(1500);
+        $cadeau3->setCategorie($categorie5);
+        $cadeau3->setDesignation('Monopoly');
+        $cadeau3->setAgeMinimum(12);
+        $cadeau3->setPrix(25);
         $manager->persist($cadeau3);
         
         $cadeau4 = new Cadeau();
-        $cadeau4->setCategorie($categorie4);
-        $cadeau4->setDesignation('Petit ours brun');
-        $cadeau4->setAgeMinimum(3);
-        $cadeau4->setPrix(10);
+        $cadeau4->setCategorie($categorie3);
+        $cadeau4->setDesignation('Nintendo Switch');
+        $cadeau4->setAgeMinimum(6);
+        $cadeau4->setPrix(300);
         $manager->persist($cadeau4);
-        
-        $cadeau5 = new Cadeau();
-        $cadeau5->setCategorie($categorie5);
-        $cadeau5->setDesignation('Billard américain');
-        $cadeau5->setAgeMinimum(5);
-        $cadeau5->setPrix(800);
-        $manager->persist($cadeau5);
+
+        /******************************************/
+        /*****************LISTES*******************/
+        /******************************************/
 
         $liste1 = new Liste();
         $liste1->setPersonne($personne2);
-        $liste1->addCadeau($cadeau4);
+        $liste1->addCadeau($cadeau1);
         $liste1->addCadeau($cadeau2);
+        $liste1->addCadeau($cadeau3);
         $manager->persist($liste1);
+
+        $liste2 = new Liste();
+        $liste2->setPersonne($personne3);
+        $liste2->addCadeau($cadeau4);
+        $manager->persist($liste2);
 
         $manager->flush();
     }
